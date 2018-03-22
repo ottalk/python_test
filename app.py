cb96de2753f7258bd766de7a4add29ea433a56f5
Kuldip Ottal <kuldip.ottal@gmail.com>
2018-03-22 08:09:29 +0000

    query vars method

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from libs.bottle import route, run
+from libs.bottle import route, run,request,template
 
 @route('/hello')
 def hello():
     return "Hello World XXX!"
+
+@route('/queryvars')
+def display_forum():
+    id = request.query.id
+    page = request.query.page or '1'
+    return template('Forum ID: {{id}} (page {{page}})', id=id, page=page)
+
 
 run(host='0.0.0.0', port=8080, debug=True)
